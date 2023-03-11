60708072dbfb73a5383984c249214c5f01cc72bd
MaxFeucht <Max.emanuel.feucht@gmail.com>
2023-03-11 16:00:52 +0100

    Added More Details and cleaned scripts

--- script.js
+++ script.js
@@ -1,97 +1,103 @@
 
 // import {createClient} from 'gptzero-js';
 
+// Funciton to randomly generate a number based on a scale
 function generateNumber(scale) {
-  return Math.floor(Math.random() * (scale - 1)) + 1
+  return Math.floor(Math.random() * scale);
 }
 
+// Function to change the color of the text (Not working currently)
 function changeColor(id, val, limit) {  
   if (val > 0.5*limit) {
   document.getElementById(id).style.color = "red";
   } 
 }
+//changeColor("facts", fact_no, limit); // That's how the function would be triggered; for some reason, the function doesn't work, i.e., the color is not changed (but no error is thrown)
 
-function handleContextMenuClick(info, tab) {
-  // Trigger the browser action click event on the tab
-  browser.tabs.sendMessage(tab.id, {action: "click_browser_action"});
-}
 
-browser.contextMenus.create(
+// Add "Investigate" to the context menu
+browser.menus.create(
   {
     id: "log-selection",
     title: "Investigate",
-    contexts: ["selection", "page"],
-    onclick: handleContextMenuClick
+    contexts: ["selection","browser_action"],
+    command: "_execute_browser_action",
+    //icon:  "icons/eye_96.png"
   });
 
+// Function to run when the extension icon is clicked
 async function fetchData() {
 
     let tabs = await browser.tabs.query({
       active: true,
       currentWindow: true});
     
+    // Add Headline (currently the Page title)
+    document.getElementById("content").innerHTML = tabs[0].title;
+
     // Facts
     var fact_limit = 10;
     var fact_no = generateNumber(fact_limit);
     document.getElementById("facts").innerHTML = fact_no;
+    document.getElementById("facts_readmore").style.display = "none";
     if (fact_no > 0.7*fact_limit) {
       document.getElementById("facts").style.color = "red";
-      } 
-    //changeColor("facts", fact_no, limit);
+      document.getElementById("facts_readmore").style.display = "block"
+      }; 
 
     var ai_limit = 100;
     var ai_no = generateNumber(ai_limit);
     document.getElementById("ai-generated").innerHTML = ai_no + "%";
+    document.getElementById("ai_readmore").style.display = "none";
     if (ai_no > 0.7*ai_limit) {
       document.getElementById("ai-generated").style.color = "red";
-      } 
+      document.getElementById("ai_readmore").style.display = "block"
+      }; 
 
     var polar_limit = 100;
     var polar_no = generateNumber(polar_limit);
     document.getElementById("polarization").innerHTML = polar_no + "%";
+    document.getElementById("polarization_readmore").style.display = "none";
     if (polar_no > 0.7*polar_limit) {
       document.getElementById("polarization").style.color = "red";
-      } 
+      document.getElementById("polarization_readmore").style.display = "block";
+      }; 
     
     var img_limit = 3;
     var img_no = generateNumber(img_limit);
-    document.getElementById("images").innerHTML = img_no + "%";
-    if (pimg_no > 0.7*img_limit) {
+    document.getElementById("images").innerHTML = img_no;
+    document.getElementById("images_readmore").style.display = "none";
+    if (img_no > 0.7*img_limit) {
       document.getElementById("images").style.color = "red";
-      } 
+      document.getElementById("images_readmore").style.display = "block"
+      };
 
     //document.getElementById("ai-generated").innerHTML= generateNumber(100) + "%";
     //document.getElementById("polarization").innerHTML= generateNumber(100) + "%";;
     //document.getElementById("images").innerHTML = generateNumber(3);//tabs[0].url; // window.getSelection().toString();
-    document.getElementById("content").innerHTML = tabs[0].title;
+    
 }
 
-
-var colorThreshold = 40,
-    spanText = document.querySelector('span');
-
-function changeColor(val) {
-    var color = "green";
-    
-    if (val > 30 && val < 60) {
-        color = "yellow";
-    } else if (val >= 60) {
-        color = "red";
-    }
-    
-    spanText.style.color = color;
-}
-
-changeColor(colorThreshold);
-
+// Function that triggers fetchData() when the extension icon is clicked
 browser.browserAction.onClicked.addListener(fetchData());
 
-browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.action === "click_browser_action") {
-    browser.browserAction.onClicked.dispatch(sender.tab);
-  }
-});
 
+///////////////////////////////////////
+// NOT NEEDED, BUT KEEPING FOR LATER
+///////////////////////////////////////
+
+function handleContextMenuClick(info, tab) {
+  // Trigger the browser action click event on the tab
+  browser.tabs.sendMessage(tab.id, {action: "click_browser_action"});
+}
+
+//changeColor(colorThreshold);
+
+//browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
+//  if (request.action === "click_browser_action") {
+//    browser.browserAction.onClicked.dispatch(sender.tab);
+//  }
+//});
 
 
 
