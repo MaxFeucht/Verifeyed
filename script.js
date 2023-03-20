9881df19eefd90b28306ed1e4ba13c2992b5ccd5
MaxFeucht <Max.emanuel.feucht@gmail.com>
2023-03-20 20:30:07 +0100

    removed API keys

--- script.js
+++ script.js
@@ -84,7 +84,7 @@ const AIDetection = async () => {
     method: 'POST',
     headers: {
       'content-type': "application/json",
-      'X-RapidAPI-Key': "5f43d03081msha5924c3989ef4b4p116059jsn168887a089b5",
+      'X-RapidAPI-Key': config.Chat_GPT_detector_key,
       'X-RapidAPI-Host': "chatgpt-detector.p.rapidapi.com"
     },
     body: '{"text":"' + selectionText + '"}' // "President Barack Obama from 2009 to 2017 before running for president in 2020. Born in Scranton, Pennsylvania in 1942, Biden has been involved in politics for more than five decades, first as a U.S. Senator from Delaware and then as Vice President. Throughout his political career, Biden has been known for his focus on issues related to the middle class and working families, including healthcare, education, and economic opportunities. He is also a strong advocate for civil rights, social justice, and international diplomacy. Since taking office in January 2021, President Biden has faced a number of challenges, including the ongoing COVID-19 pandemic, economic recovery, and social justice issues. He has worked to implement policies and programs aimed at addressing these challenges, including a massive infrastructure plan and a vaccine distribution strategy."}'
@@ -146,7 +146,7 @@ const AIDetection = async () => {
       method: 'POST',
       headers: {
         'content-type': "application/json",
-        'X-RapidAPI-Key': "5f43d03081msha5924c3989ef4b4p116059jsn168887a089b5",
+        'X-RapidAPI-Key': config.Chat_GPT_detector_key,
         'X-RapidAPI-Host': "chatgpt-detector.p.rapidapi.com"
       },
       body: '{"text":"' + selectionText + '"}' // "President Barack Obama from 2009 to 2017 before running for president in 2020. Born in Scranton, Pennsylvania in 1942, Biden has been involved in politics for more than five decades, first as a U.S. Senator from Delaware and then as Vice President. Throughout his political career, Biden has been known for his focus on issues related to the middle class and working families, including healthcare, education, and economic opportunities. He is also a strong advocate for civil rights, social justice, and international diplomacy. Since taking office in January 2021, President Biden has faced a number of challenges, including the ongoing COVID-19 pandemic, economic recovery, and social justice issues. He has worked to implement policies and programs aimed at addressing these challenges, including a massive infrastructure plan and a vaccine distribution strategy."}'
